7ecf9e6f26a744d2f767235cafcad9aaccabcda5
thebestyou <839825915@qq.com>
2017-08-12 21:35:36 +0800

    change in mac in 2017/8/12

--- Code/Calculate.java
+++ Code/Calculate.java
@@ -1,6 +1,9 @@
 package Code;
 
 import java.util.*;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
 import java.io.*;
 import java.time.*;
 
@@ -8,7 +11,8 @@ public class Calculate {
 	private static PrintWriter pw;
 	private static BufferedReader rd;
 	private static String timeNow = YearMonth.now() + "／" + LocalTime.now();
-
+	private static boolean ifTheFileReadSuccessfully	 = false;
+	private static StringBuffer Document;
 	public static void main(String[] args) {
 		begin();
 	}
@@ -16,13 +20,17 @@ public class Calculate {
 	private static void begin() {
 		// Determine if the "配置.ini" is already exists
 		try {
-			rd = new BufferedReader(new InputStreamReader(new FileInputStream("./配置.ini")));
+			rd = new BufferedReader(
+					new InputStreamReader(
+							new FileInputStream("./配置.ini")));
 			System.out.println("文件读取成功");
 		} catch (IOException e) {
 			System.out.println("请在\"配置.ini\"下填写");
 			try {
 				pw = new PrintWriter(
-						new BufferedWriter(new OutputStreamWriter(new FileOutputStream(".//配置.ini"), "utf8")));
+						new BufferedWriter(
+								new OutputStreamWriter(
+										new FileOutputStream(".//配置.ini"), "utf8")));
 				System.out.println("文件创建成功");
 				System.out.println();
 				Document dc = new Document("创建时间--" + timeNow.substring(0, 13));
@@ -37,5 +45,17 @@ public class Calculate {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	private static boolean IfFileReadRight() throws IOException{
+		String str = null;
+			while((str = rd.readLine()) != null){
+					Document.append(str);
+			}
+		//remove space
+		String str1 = new String(Document);
+		str1 = str1.replace("","");
+		//System.out.println(str1);
+		//to JudgeFileReadRight
+		return true;
 	}
 }
