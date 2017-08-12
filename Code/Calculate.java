1eb02a826dba3a574a751527947ab87895cb8577
thebestyou <839825915@qq.com>
2017-08-12 18:09:34 +0800

    chang in mac in2017/8/11

--- Code/Calculate.java
+++ Code/Calculate.java
@@ -1,38 +1,40 @@
 package Code;
+
 import java.util.*;
 import java.io.*;
 import java.time.*;
 
-public class Calculate{
+public class Calculate {
 	private static PrintWriter pw;
 	private static BufferedReader rd;
-	private static String timeNow = YearMonth.now() +" "+ LocalTime.now();
+	private static String timeNow = YearMonth.now() + " " + LocalTime.now();
+
 	public static void main(String[] args) {
-	//Determine if the "配置.ini" is already exists
-		try{
-			rd = new BufferedReader(
-				new InputStreamReader(
-					new FileInputStream("配置.ini")));
+		begin();
+	}
+
+	private static void begin() {
+		// Determine if the "配置.ini" is already exists
+		try {
+			rd = new BufferedReader(new InputStreamReader(new FileInputStream("./配置.ini")));
 			System.out.println("文件读取成功");
-		}catch(IOException e){	
+		} catch (IOException e) {
 			System.out.println("请在\"配置.ini\"下填写");
-			try{
+			try {
 				pw = new PrintWriter(
-					new BufferedWriter(
-						new OutputStreamWriter(
-							new FileOutputStream("配置.ini"),"utf8")));						
+						new BufferedWriter(new OutputStreamWriter(new FileOutputStream(".//配置.ini"), "utf8")));
 				System.out.println("文件创建成功");
 				System.out.println();
-				pw.print("创建时间");
-				pw.println(YearMonth.now() +" "+ LocalTime.now());
+				pw.print("创建时间--");
+				pw.println(timeNow.substring(0, 13));
 				pw.close();
-			}catch(IOException a){
+			} catch (IOException a) {
 				System.out.println("文件创建失败");
-			}catch(Exception a){
+			} catch (Exception a) {
 				System.out.println("功能需要更新");
 				a.printStackTrace();
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
