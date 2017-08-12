b23fb53d41715bf1da3f863179f976caf9a2cf24
thebestyou <839825915@qq.com>
2017-08-12 15:14:57 +0800

    change in mac in 2017/8/12 0

--- Calculate.java
+++ Calculate.java
@@ -1,27 +1,33 @@
 import java.util.*;
 import java.io.*;
+import java.time.*;
 
-public static void main(String[] args) {
+public class Calculate{
+	public static void main(String[] args) {
 	//Determine if the "配置.ini" is already exists
-	try{
-		FileReader fr = new FileReader(
-			new BufferedReader(
-				new InputStreamReader(
-					new InputStream("配置.ini"))));
-		System.out.println("文件读取成功");
-	}catch(IOException e){
-		System.out.println("请在\"配置.ini\"下填写");
 		try{
-			PrintWriter pw = new PrintWriter(
-				new BufferedWriter(
-					new InputStreamWriter("配置.ini")));
-			System.out.println("文件创建成功");
-		}catch(IOException a){
-
-		}catch(Expection a){
-			
+			BufferedReader rd = new BufferedReader(
+				new InputStreamReader(
+					new FileInputStream("配置.ini")));
+			System.out.println("文件读取成功");
+		}catch(IOException e){	
+			System.out.println("请在\"配置.ini\"下填写");
+			try{
+				PrintWriter pw = new PrintWriter(
+					new BufferedWriter(
+						new OutputStreamWriter(
+							new FileOutputStream("配置.ini"))));						
+				System.out.println("文件创建成功");
+				pw.print("创建时间");
+				pw.println(LocalDateTime.now());
+			}catch(IOException a){
+				System.out.println("文件创建失败");
+			}catch(Exception a){
+				System.out.println("功能需要更新");
+				a.printStackTrace();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
 		}
-	}catch(Expection e){
-		e.printStackTrace();
 	}
 }
