35dd9bd0624771d97a850876b707ab8394d435ee
thebestyou <839825915@qq.com>
2017-08-12 16:50:17 +0800

    chang in Eclipse

--- Calculate.java
+++ Calculate.java
@@ -3,23 +3,28 @@ import java.io.*;
 import java.time.*;
 
 public class Calculate{
+	private static PrintWriter pw;
+	private static BufferedReader rd;
+
 	public static void main(String[] args) {
 	//Determine if the "配置.ini" is already exists
 		try{
-			BufferedReader rd = new BufferedReader(
+			rd = new BufferedReader(
 				new InputStreamReader(
 					new FileInputStream("配置.ini")));
 			System.out.println("文件读取成功");
 		}catch(IOException e){	
 			System.out.println("请在\"配置.ini\"下填写");
 			try{
-				PrintWriter pw = new PrintWriter(
+				pw = new PrintWriter(
 					new BufferedWriter(
 						new OutputStreamWriter(
-							new FileOutputStream("配置.ini"))));						
+							new FileOutputStream("配置.ini"),"utf8")));						
 				System.out.println("文件创建成功");
+				System.out.println(YearMonth.now() +" "+ LocalTime.now());
 				pw.print("创建时间");
-				pw.println(LocalDateTime.now());
+				pw.println(YearMonth.now() +" "+ LocalTime.now());
+				pw.close();
 			}catch(IOException a){
 				System.out.println("文件创建失败");
 			}catch(Exception a){
---
+++ Document.java
@@ -0,0 +1,9 @@
+public class Document {
+	@Override
+	public String toString(){
+		return "Name      Price"
+			 + "无色小晶体："
+			 + "白色小晶体";
+		
+	}
+}
